ad614edf5e8704fcb26c6d26e7a107e3404fa2d4
Kate Cevora <kate.wilkinson@illumr.com>
2024-10-24 16:27:03 +0100

    First commit

---
+++ testGeneralisation.py
@@ -0,0 +1,153 @@
+# Takes a parameters file, runs a generalisation experiment and measures diversity of datastets
+import argparse
+import mlflow
+import os
+import torchvision.transforms as transforms
+from trainers import Trainer, TrainerParams, Inference
+from autoencoder2D import ConvAutoencoder
+from classifier import ImageClassifier
+from torch.utils.data import Subset
+from torchvision import datasets
+from diversityScore import DiversityScore
+import pickle as pkl
+from datasetUtils import generateSubsetIndex, generateSubsetIndexDiverse, RotationTransform
+import plotting
+import numpy as np
+from medMNISTDataset import MedNISTDataset
+
+# Set up the argument parser
+parser = argparse.ArgumentParser(description="Calculate the generalisation ability and diversity scores for a dataset")
+parser.add_argument("-e", "--experiment", type=str, help="Name of the experiment.", default="GeneralisationMinMaxDiversity")
+parser.add_argument("-p", "--params_file", type=str, help="Name of params file.", default="test_params.pkl")
+parser.add_argument("-r", "--root_dir", type=str, help="Root directory where the code and data are located", default="/Users/katecevora/Documents/PhD")
+
+args = parser.parse_args()
+
+root_dir = args.root_dir
+code_dir = os.path.join(root_dir, "code/AutoencoderMNIST")
+data_dir = os.path.join(root_dir, "data")
+experiment_name = args.experiment
+
+assert experiment_name in ["Generalisation_Fixed_Entropy", "GeneralisationMinMaxDiversity"], "Experiment name is not recognised"
+
+params_file_path = os.path.join(code_dir, "params", experiment_name, args.params_file)
+models_path = os.path.join(code_dir, "models")
+model_save_path = os.path.join(code_dir, "models")
+loss_plot_save_path = os.path.join(code_dir, "loss.png")
+
+# number of test/valid dataset samples per category
+number_test_samples_per_cat = 500
+
+# convert data to torch.FloatTensor
+transform_mnist = transforms.ToTensor()
+transform_emnist = transforms.Compose([transforms.ToTensor(), RotationTransform(-270)])
+
+# Set our tracking server uri for logging with MLFlow
+mlflow.set_tracking_uri(uri="http://127.0.0.1:8080")
+
+# Create a new MLflow Experiment
+mlflow.set_experiment(experiment_name)
+
+
+def getDataSubsets(data, n_samples, diversity="high"):
+    subset_idx = generateSubsetIndexDiverse(data, "all", n_samples, diversity=diversity)
+
+    return Subset(data, subset_idx)
+
+def main():
+    # open a params file
+    assert os.path.exists(params_file_path), "The path {} to the params file does not exist.".format(params_file_path)
+
+    f = open(params_file_path, "rb")
+    params = pkl.load(f)
+    f.close()
+
+    # check the params file
+    assert isinstance(params, dict), f"Expected a dictionary, but got {type(params).__name__}"
+
+    # check what dataset we are using and load the data
+    dataset_name = params["dataset_name"]
+    assert isinstance(dataset_name, str), "Dataset name must be a string."
+    assert dataset_name in ["MNIST", "EMNIST", "PneuNIST"], "The dataset name {} is not recognised."
+
+    print("Starting TestGeneralisation {0} experiment with {1} dataset".format(experiment_name, dataset_name))
+
+    train_data = MedNISTDataset(data_dir, split="train", task="pneumoniamnist")
+    valid_data = MedNISTDataset(data_dir, split="val", task="pneumoniamnist")
+    test_data = MedNISTDataset(data_dir, split="test", task="pneumoniamnist")
+    out_features = 2
+    ae_model_name = "autoencoderMedMNISTfull.pt"
+
+    print("Finished loading data.")
+
+    if len(train_data) < params["n_samples"] * 5:
+        print("Warning: train data has {0} samples not {1}".format(len(train_data), params["n_samples"] * 5))
+    if len(valid_data) < number_test_samples_per_cat * 10:
+        print("Warning: validation data has {0} samples not {1}".format(len(valid_data), number_test_samples_per_cat * 10))
+    if len(test_data) < number_test_samples_per_cat * 10:
+        print("Warning: test data has {0} samples not {1}".format(len(test_data), number_test_samples_per_cat * 10))
+
+    # First randomly select a subset so that we don't have to compute a massive similarity matrix
+    idx_train = generateSubsetIndex(train_data, "all", min(params["n_samples"] * 5, len(train_data)), params["random_seed"])
+    idx_valid = generateSubsetIndex(valid_data, "all", min(number_test_samples_per_cat * 10, len(valid_data)), params["random_seed"])
+    idx_test = generateSubsetIndex(test_data, "all", min(number_test_samples_per_cat * 10, len(test_data)), params["random_seed"])
+
+    train_data = Subset(train_data, idx_train)
+    valid_data = Subset(valid_data, idx_valid)
+    test_data = Subset(test_data, idx_test)
+
+    # then choose maximally or minimally diverse samples from the training subset
+    #train_data = getDataSubsets(train_data, params["n_samples"], diversity=params["diversity"])
+
+    print("Finished sampling data.")
+
+    # load the AE model that we will use to embed the data
+    model_ae = ConvAutoencoder(save_path=os.path.join(models_path, ae_model_name))
+
+    trainer_params = TrainerParams(n_epochs=params["n_epochs"], num_workers=params["n_workers"],
+                                   batch_size=params["batch_size"])
+
+    # diversity score all datasets
+    ds_train = DiversityScore(train_data, trainer_params, model_ae)
+    ds_test = DiversityScore(test_data, trainer_params, model_ae)
+    ds_valid = DiversityScore(valid_data, trainer_params, model_ae)
+
+    train_scores = ds_train.scoreDiversity()
+    test_scores = ds_test.scoreDiversity()
+    valid_scores = ds_valid.scoreDiversity()
+
+    print("Training ResNet for classification.")
+
+    from trainResNet import runTraining
+
+    runTraining("pneumoniamnist", './output', 3, 128, 28, True, 'resnet18', True, True, None, 'model1')
+
+    print("Finished experiment.")
+
+    # record everything in MLFlow
+    with mlflow.start_run():
+        # Log the hyperparameters
+        print("Starting mlflow logging")
+        mlflow.log_params(params)
+
+        # Log the diversity metrics
+        for (ds, scores) in zip(["train", "test", "valid"], [train_scores, test_scores, valid_scores]):
+            for s in ["vs", "av_sim", "intdiv"]:
+                mlflow.log_metric("{0}_pixel_{1}".format(s, ds), scores["{}_pixel".format(s)])
+                mlflow.log_metric("{0}_embed_full_{1}".format(s, ds), scores["{}_auto".format(s)])
+                mlflow.log_metric("{0}_inception_{1}".format(s, ds), scores["{}_inception".format(s)])
+
+            mlflow.log_metric("vs_entropy_{}".format(ds), scores["label_entropy"])
+
+        # log the generalisation accuracy
+        mlflow.log_metric("test_accuracy", test_accuracy)
+        mlflow.log_metric("valid_accuracy", valid_accuracy)
+
+        # log the loss plot for the classification model
+        #mlflow.log_artifact(loss_plot_save_path)
+
+    print("Finished mlflow logging.")
+
+
+if __name__ == "__main__":
+    main()
---
+++ trainResNet.py
@@ -0,0 +1,279 @@
+import argparse
+import os
+import time
+from collections import OrderedDict
+from copy import deepcopy
+
+import medmnist
+import PIL
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as data
+from torch.utils.data import Subset
+import torchvision.transforms as transforms
+from medmnist import INFO
+from evaluatorLocal import Evaluator
+from models import ResNet18, ResNet50
+import numpy as np
+from tqdm import trange
+
+
+def runTraining(subset_idx, data_flag, output_root, num_epochs, batch_size, size, download, model_flag, as_rgb, model_path, run):
+
+    lr = 0.001
+    gamma = 0.1
+    milestones = [0.5 * num_epochs, 0.75 * num_epochs]
+
+    info = INFO[data_flag]
+    task = info['task']
+    n_channels = 3 if as_rgb else info['n_channels']
+    n_classes = len(info['label'])
+
+    DataClass = getattr(medmnist, info['python_class'])
+
+    # Check if we have a GPU
+    if torch.cuda.is_available():
+        device = torch.device('cuda')
+    else:
+        device = torch.device('cpu')
+
+    output_root = os.path.join(output_root, data_flag, time.strftime("%y%m%d_%H%M%S"))
+
+    if not os.path.exists(output_root):
+        os.makedirs(output_root)
+
+    print('==> Preparing data...')
+
+    data_transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=[.5], std=[.5])])
+
+    train_dataset = Subset(DataClass(split='train', transform=data_transform, download=download, as_rgb=as_rgb, size=size), subset_idx)
+    val_dataset = DataClass(split='val', transform=data_transform, download=download, as_rgb=as_rgb, size=size)
+    test_dataset = DataClass(split='test', transform=data_transform, download=download, as_rgb=as_rgb, size=size)
+
+    train_loader = data.DataLoader(dataset=train_dataset,
+                                   batch_size=batch_size,
+                                   shuffle=True)
+    train_loader_at_eval = data.DataLoader(dataset=train_dataset,
+                                           batch_size=batch_size,
+                                           shuffle=False)
+    val_loader = data.DataLoader(dataset=val_dataset,
+                                 batch_size=batch_size,
+                                 shuffle=False)
+    test_loader = data.DataLoader(dataset=test_dataset,
+                                  batch_size=batch_size,
+                                  shuffle=False)
+
+    print('==> Building and training model...')
+
+    if model_flag == 'resnet18':
+        model = ResNet18(in_channels=n_channels, num_classes=n_classes)
+    elif model_flag == 'resnet50':
+        model = ResNet50(in_channels=n_channels, num_classes=n_classes)
+    else:
+        raise NotImplementedError
+
+    model = model.to(device)
+
+    train_evaluator = Evaluator(data_flag, 'train', size=size)
+    val_evaluator = Evaluator(data_flag, 'val', size=size)
+    test_evaluator = Evaluator(data_flag, 'test', size=size)
+
+    if task == "multi-label, binary-class":
+        criterion = nn.BCEWithLogitsLoss()
+    else:
+        criterion = nn.CrossEntropyLoss()
+
+    if model_path is not None:
+        model.load_state_dict(torch.load(model_path, map_location=device)['net'], strict=True)
+        train_metrics = test(model, train_evaluator, train_loader_at_eval, task, criterion, device, run, output_root)
+        val_metrics = test(model, val_evaluator, val_loader, task, criterion, device, run, output_root)
+        test_metrics = test(model, test_evaluator, test_loader, task, criterion, device, run, output_root)
+
+        print('train  auc: %.5f  acc: %.5f\n' % (train_metrics[1], train_metrics[2]) + \
+              'val  auc: %.5f  acc: %.5f\n' % (val_metrics[1], val_metrics[2]) + \
+              'test  auc: %.5f  acc: %.5f\n' % (test_metrics[1], test_metrics[2]))
+
+    if num_epochs == 0:
+        return
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=milestones, gamma=gamma)
+
+    logs = ['loss', 'auc', 'acc']
+    train_logs = ['train_' + log for log in logs]
+    val_logs = ['val_' + log for log in logs]
+    test_logs = ['test_' + log for log in logs]
+    log_dict = OrderedDict.fromkeys(train_logs + val_logs + test_logs, 0)
+
+    best_auc = 0
+    best_epoch = 0
+    best_model = deepcopy(model)
+
+    global iteration
+    iteration = 0
+
+    for epoch in trange(num_epochs):
+        train_loss = train(model, train_loader, task, criterion, optimizer, device)
+
+        train_metrics = test(model, train_evaluator, train_loader_at_eval, task, criterion, device, run)
+        val_metrics = test(model, val_evaluator, val_loader, task, criterion, device, run)
+        test_metrics = test(model, test_evaluator, test_loader, task, criterion, device, run)
+
+        scheduler.step()
+
+        for i, key in enumerate(train_logs):
+            log_dict[key] = train_metrics[i]
+        for i, key in enumerate(val_logs):
+            log_dict[key] = val_metrics[i]
+        for i, key in enumerate(test_logs):
+            log_dict[key] = test_metrics[i]
+
+        # for key, value in log_dict.items():
+        #    writer.add_scalar(key, value, epoch)
+
+        cur_auc = val_metrics[1]
+        if cur_auc > best_auc:
+            best_epoch = epoch
+            best_auc = cur_auc
+            best_model = deepcopy(model)
+            print('cur_best_auc:', best_auc)
+            print('cur_best_epoch', best_epoch)
+
+    state = {
+        'net': best_model.state_dict(),
+    }
+
+    path = os.path.join(output_root, 'best_model.pth')
+    torch.save(state, path)
+
+    train_metrics = test(best_model, train_evaluator, train_loader_at_eval, task, criterion, device, run, output_root)
+    val_metrics = test(best_model, val_evaluator, val_loader, task, criterion, device, run, output_root)
+    test_metrics = test(best_model, test_evaluator, test_loader, task, criterion, device, run, output_root)
+
+    train_log = 'train  auc: %.5f  acc: %.5f\n' % (train_metrics[1], train_metrics[2])
+    val_log = 'val  auc: %.5f  acc: %.5f\n' % (val_metrics[1], val_metrics[2])
+    test_log = 'test  auc: %.5f  acc: %.5f\n' % (test_metrics[1], test_metrics[2])
+
+    log = '%s\n' % (data_flag) + train_log + val_log + test_log
+    print(log)
+
+
+def train(model, train_loader, task, criterion, optimizer, device):
+    total_loss = []
+    global iteration
+
+    model.train()
+    for batch_idx, (inputs, targets) in enumerate(train_loader):
+        optimizer.zero_grad()
+        outputs = model(inputs.to(device))
+
+        if task == 'multi-label, binary-class':
+            targets = targets.to(torch.float32).to(device)
+            loss = criterion(outputs, targets)
+        else:
+            targets = torch.squeeze(targets, 1).long().to(device)
+            loss = criterion(outputs, targets)
+
+        total_loss.append(loss.item())
+        # writer.add_scalar('train_loss_logs', loss.item(), iteration)
+        print('train_loss_{0:.3f}, epoch: {1}'.format(loss.item(), iteration))
+        iteration += 1
+
+        loss.backward()
+        optimizer.step()
+
+    epoch_loss = sum(total_loss) / len(total_loss)
+    return epoch_loss
+
+
+def test(model, evaluator, data_loader, task, criterion, device, run, save_folder=None):
+    model.eval()
+
+    total_loss = []
+    y_score = torch.tensor([]).to(device)
+    y_targets = torch.tensor([]).to(device)
+
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(data_loader):
+            outputs = model(inputs.to(device))
+
+            if task == 'multi-label, binary-class':
+                targets = targets.to(torch.float32).to(device)
+                loss = criterion(outputs, targets)
+                m = nn.Sigmoid()
+                outputs = m(outputs).to(device)
+            else:
+                targets = torch.squeeze(targets, 1).long().to(device)
+                loss = criterion(outputs, targets)
+                m = nn.Softmax(dim=1)
+                outputs = m(outputs).to(device)
+                targets = targets.float().resize_(len(targets), 1)
+
+            total_loss.append(loss.item())
+            y_score = torch.cat((y_score, outputs), 0)
+            y_targets = torch.cat((y_targets, targets))
+
+        y_score = y_score.detach().cpu().numpy()
+        y_targets = y_targets.detach().cpu().numpy()
+        auc, acc = evaluator.evaluate(y_score, y_targets, save_folder, run)
+
+        test_loss = sum(total_loss) / len(total_loss)
+
+        return [test_loss, auc, acc]
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='RUN Baseline model of MedMNIST2D')
+
+    parser.add_argument('--data_flag',
+                        default='pneumoniamnist',
+                        type=str)
+    parser.add_argument('--output_root',
+                        default='./output',
+                        help='output root, where to save models and results',
+                        type=str)
+    parser.add_argument('--num_epochs',
+                        default=100,
+                        help='num of epochs of training, the script would only test model if set num_epochs to 0',
+                        type=int)
+    parser.add_argument('--size',
+                        default=28,
+                        help='the image size of the dataset, 28 or 64 or 128 or 224, default=28',
+                        type=int)
+    parser.add_argument('--batch_size',
+                        default=128,
+                        type=int)
+    parser.add_argument('--download',
+                        action="store_true")
+    parser.add_argument('--as_rgb',
+                        help='convert the grayscale image to RGB',
+                        action="store_true")
+    parser.add_argument('--model_path',
+                        default=None,
+                        help='root of the pretrained model to test',
+                        type=str)
+    parser.add_argument('--model_flag',
+                        default='resnet18',
+                        help='choose backbone from resnet18, resnet50',
+                        type=str)
+    parser.add_argument('--run',
+                        default='model1',
+                        help='to name a standard evaluation csv file, named as {flag}_{split}_[AUC]{auc:.3f}_[ACC]{acc:.3f}@{run}.csv',
+                        type=str)
+
+    args = parser.parse_args()
+    data_flag = args.data_flag
+    output_root = args.output_root
+    num_epochs = args.num_epochs
+    size = args.size
+    batch_size = args.batch_size
+    download = args.download
+    model_flag = args.model_flag
+    as_rgb = args.as_rgb
+    model_path = args.model_path
+    run = args.run
+    subset_idx = np.arange(1, 1000)
+
+    runTraining(subset_idx, data_flag, output_root, num_epochs, batch_size, size, download, model_flag, as_rgb, model_path, run)
