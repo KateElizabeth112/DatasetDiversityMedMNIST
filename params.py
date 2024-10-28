95c7edd4bf604acaf04aae085e82a7cf7771a64a
Kate Cevora <kate.wilkinson@illumr.com>
2024-10-28 14:43:06 +0000

    TrainerParams class is no longer needed

--- params.py
+++
@@ -1,5 +0,0 @@
-class TrainerParams:
-    def __init__(self, n_epochs=30, num_workers=0, batch_size=20):
-        self.n_epochs = n_epochs
-        self.num_workers = num_workers
-        self.batch_size = batch_size
