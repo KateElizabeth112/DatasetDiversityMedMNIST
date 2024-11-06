2c279f2892a920619160cae57ea189549ebf1c66
Kate Cevora <kate.wilkinson@illumr.com>
2024-11-06 13:21:42 +0000

    Set up to run images of size 28

--- runExperimentHPC.sh
+++ runExperimentHPC.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #PBS -l walltime=12:00:00
 #PBS -l select=1:ncpus=15:mem=120gb
-#PBS -N pneumoniamnist_200_128
+#PBS -N pneumoniamnist_200_28
 
 # bash script to run generalisation experiments on HPC
-
 cd ${PBS_O_WORKDIR}
 
 # Launch virtual environment
@@ -14,4 +13,4 @@ module load anaconda3/personal
 #pip install -r requirements.txt
 
 # run experiments
-python runExperiment.py -r "/rds/general/user/kc2322/home/" -n 200 -d "pneumoniamnist" -i 128
+python runExperiment.py -r "/rds/general/user/kc2322/home/" -n 200 -d "pneumoniamnist" -i 28
