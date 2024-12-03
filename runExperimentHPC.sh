91779764f3deee0f5b6d1d460e9aeeeb6108b8de
Kate Cevora <kate.wilkinson@illumr.com>
2024-12-03 19:41:49 +0000

    Prepared for chestmnist128 ns=50

--- runExperimentHPC.sh
+++ runExperimentHPC.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -l walltime=48:00:00
 #PBS -l select=1:ncpus=15:mem=80gb:ngpus=1:gpu_type=RTX6000
-#PBS -N chestmnist_200_128
+#PBS -N chestmnist_50_128
 
 # bash script to run generalisation experiments on HPC
 cd ${PBS_O_WORKDIR}
@@ -13,4 +13,4 @@ module load anaconda3/personal
 #pip install -r requirements.txt
 
 # run experiments
-python runExperiment.py -r "/rds/general/user/kc2322/home/" -n 200 -d "chestmnist" -i 128
+python runExperiment.py -r "/rds/general/user/kc2322/home/" -n 50 -d "chestmnist" -i 128
