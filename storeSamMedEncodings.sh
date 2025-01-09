88d87bff03513824657d732ad9f403cb5b564f1b
Kate Cevora <kate.wilkinson@illumr.com>
2025-01-09 11:59:26 +0000

    Set up to encode breastmnist28

--- storeSamMedEncodings.sh
+++ storeSamMedEncodings.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-#PBS -l walltime=48:00:00
+#PBS -l walltime=24:00:00
 #PBS -l select=1:ncpus=15:mem=120gb
-#PBS -N sammed_encode_chestmnist_128
+#PBS -N sammed_encode_breastmnist_28
 
-# bash script to run generalisation experiments on HPC
 cd ${PBS_O_WORKDIR}
 
 # Launch virtual environment
 module load anaconda3/personal
 
 # run experiments
-python storeSamMedEncodings.py -r "/rds/general/user/kc2322/home/" -d "chestmnist" -i 128 -s 57400
+python storeSamMedEncodings.py -r "/rds/general/user/kc2322/home/" -d "breastmnist" -i 28
