cf483a208953476c730c8fb184c82904c73efa50
Kate Cevora <kate.wilkinson@illumr.com>
2025-01-09 14:24:34 +0000

    Updated to breastmnist128

--- storeSamMedEncodings.sh
+++ storeSamMedEncodings.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -l walltime=24:00:00
 #PBS -l select=1:ncpus=15:mem=120gb
-#PBS -N sammed_encode_breastmnist_28
+#PBS -N sammed_encode_breastmnist_128
 
 cd ${PBS_O_WORKDIR}
 
@@ -9,4 +9,4 @@ cd ${PBS_O_WORKDIR}
 module load anaconda3/personal
 
 # run experiments
-python storeSamMedEncodings.py -r "/rds/general/user/kc2322/home/" -d "breastmnist" -i 28
+python storeSamMedEncodings.py -r "/rds/general/user/kc2322/home/" -d "breastmnist" -i 128
