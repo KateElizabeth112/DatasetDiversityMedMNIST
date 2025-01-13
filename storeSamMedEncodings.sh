35b10fbf53c3edc49295883706bdcb3492da32f5
Kate Cevora <kate.wilkinson@illumr.com>
2025-01-13 14:38:46 +0000

    Octmnist sammed encodings round 2

--- storeSamMedEncodings.sh
+++ storeSamMedEncodings.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -l walltime=71:00:00
+#PBS -l walltime=48:00:00
 #PBS -l select=1:ncpus=15:mem=120gb
 #PBS -N sammed_encode_octmnist_28
 
@@ -9,4 +9,4 @@ cd ${PBS_O_WORKDIR}
 module load anaconda3/personal
 
 # run experiments
-python storeSamMedEncodings.py -r "/rds/general/user/kc2322/home/" -d "octmnist" -i 28
+python storeSamMedEncodings.py -r "/rds/general/user/kc2322/home/" -d "octmnist" -i 28 -s 77221
